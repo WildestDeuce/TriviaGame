6a63c86326a2f8314509208d374ad678dad1a0c1
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-06-15 23:01:05 -0400

    js and css files added begin timer functions

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+//prerequisites
+getMinutes();
+getSeconds();
+setInterval();
+
+//variables and functions
+var mins = 2;
+var secs = mins * 60;
+
+function countdown(){
+setTimeout('decrement()', 60,);
+}
+
+function decrement(){
+    if (document.getElementById) {
+        minutes = $("#minutes");
+        seconds = $("#seconds")
+    }
+}
