631c24601a5ff1854cf4614ac7ec6e04efc73d80
Charles Mercer <c.e.mercer15@gmail.com>
2019-06-22 22:58:58 -0400

    #ptrivia has questions and choices

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,115 +1,149 @@
 //variables and functions
 
-$(document).ready(function () {
-    var timer = 20;
-    var timerDIv = $("#count-down");
-    timerDIv.html(`${timer}`);
-})
+
+var timer = 3;
+var timerDIv = $("#count-down");
 var running = false
 var intervalId;
-var endTime = 20
+var endTime = 3
+var questionNumber = 0;
 
-$("#startbutton").on("click",countDown)
+$(document).ready(main);
+function main() {
+    timerDIv.html(`${timer}`);
+    $("#startbutton").on("click", startQuiz);
+}
 
-
-    
 //call funtion countDown somwhere in code 
-function countDown() {
+function startQuiz() {
+    console.log(">startQuiz");
     //set timer 
-    timer = 20;
+    timer = endTime;
     //in not running set to true, clear the interval and start decrement function
     if (!running) {
         intervalId = setInterval(decrement, 1000);
         running = true;
         $("#count-down").text(timer)
     }
+    questionNumber = 0;
+    paintQuestion(questionNumber);
+    paintChoice(questionNumber)
+    console.log("<startQuiz");
+
 };
+
 function decrement() {
+    console.log(">decrement")
     //counts timer down
     timer--;
     //adds to html
-$("#count-down").text(timer); 
-        // if time runs out, run stop function, show in html time ran out and add score to out of time
-        if (timer === 0) {
-            //stop();
-            endTime++;
-            clear();
-            //timedOut();
-            console.log("out of time!");
-        }
-    
+    $("#count-down").text(timer);
+    // if time runs out, run stop function, show in html time ran out and add score to out of time
+    if (timer === 0) {
+        timeOut();
+        nextQuestion();
+    }
+    console.log("<decrement")
 };
 
-function clear() {
-
-    $("#ptrivia").empty()
-    clearInterval(intervalId)
+function nextQuestion() {
+    clearInterval(intervalId);
+    timer = endTime;
+    questionNumber++;
+    paintQuestion(questionNumber);
+    paintChoice(questionNumber);
+    intervalId = setInterval(decrement, 1000);
+    running = true;
+    $("#count-down").text(timer)
 }
 
-//for ()
-//questions [i].choices [questions [i].answer]'
+/*function nextChoice() {
+    clearInterval(intervalId);
+    timer = endTime;
+    intervalId = setInterval(decrement, 1000);
+    running = true;
+    $("#count-down").text(timer)
+}*/
+
+function timeOut() {
+    console.log("out of time!");
+}
 
 //every question in pink box of array .append to pink box
-/*var questions =
+var qArr =
     [
         {
             question: "What is a pregnant goldfish called?", // A Twit
-            choices: ["A float", "A crank", "A tramp", "A twit"];
+            choices: ["A float", "A crank", "A tramp", "A twit"],
             answer: 3
         },
 
         {
-            question: "Whos is the legendary Benedictine monk whom invented champagne?"  //Dom Perignon
-            choices: ["Perrier Jouet", "Dom Perignon", "Armand De Brignac", "Louis Roederer"];
+            question: "Whos is the legendary Benedictine monk whom invented champagne?", //Dom Perignon
+            choices: ["Perrier Jouet", "Dom Perignon", "Armand De Brignac", "Louis Roederer"],
             answer: 1
         },
         {
 
             question: "This the largest fresh water lake in the world", //Lake Superior
-            choices: ["Lake Superior", "Lake Victoria", "Loch Ness", "Lake Bikal"]
+            choices: ["Lake Superior", "Lake Victoria", "Loch Ness", "Lake Bikal"],
             answer: 0
         },
         {
-            question: "Who was the first actor to play James Bond in the movie series?" //Sir Sean Connery
-            choices: ["Roger Moore", "Daniel Craig", "Sean Connery", "Pierce Brosnan"]
+            question: "Who was the first actor to play James Bond in the movie series?", //Sir Sean Connery
+            choices: ["Roger Moore", "Daniel Craig", "Sean Connery", "Pierce Brosnan"],
             answer: 2
         },
         {
-            question: "What is a lexicon?" //A Dictionary
-            choices: ["A dictionary", "An hourglass", "A pocketwatch", "A boat"]
+            question: "What is a lexicon?", //A Dictionary
+            choices: ["A dictionary", "An hourglass", "A pocketwatch", "A boat"],
             answer: 0
         },
-        {    
-            question: "This fighter has a career record of 49 wins ZERO losses."; //Rocky Marciano
-            choices: ["Floyd Mayweather", "Muhammad Ali", "Rockey Marciano", "Sugar Ray Leonard"]
+        {
+            question: "This fighter has a career record of 49 wins ZERO losses.",//Rocky Marciano
+            choices: ["Floyd Mayweather", "Muhammad Ali", "Rockey Marciano", "Sugar Ray Leonard"],
             answer: 2
         },
         {
-            question: "What percent of an egg's weight is its shell?"; //12%
-            choices: ["9%", "10%", "11%", "12%"]
+            question: "What percent of an egg's weight is its shell?", //12%
+            choices: ["9%", "10%", "11%", "12%"],
             answer: 3
         },
         {
-            question: "What is melissaphobia?"; //The fear of bees
-            choices: ["Fear of clouds", "Fear of bees", "Fear of words", "Fear of fear"]
+            question: "What is melissaphobia?", //The fear of bees
+            choices: ["Fear of clouds", "Fear of bees", "Fear of words", "Fear of fear"],
             answer: 1
-        },    
-    
+        },
+
         {
-            question: "Who was the first baseball player to have his jersey retired?" //Lou Gehrig 
-            choices: ["Mickey Mantle", "Babe Ruth", "Ted Williams", "Lou Gehrig"]
+            question: "Who was the first baseball player to have his jersey retired?", //Lou Gehrig 
+            choices: ["Mickey Mantle", "Babe Ruth", "Ted Williams", "Lou Gehrig"],
             answer: 3
-        }    
+        },
         {
-            qeustion: "This is the Greek God of dreams" //Morpheus
-            choices: ["Morpheus", "Demeter", "Dionysus", "Chiron"]
+            question: "This is the Greek God of dreams", //Morpheus
+            choices: ["Morpheus", "Demeter", "Dionysus", "Chiron"],
             answer: 1
 
         }
-
-
-        
     ]
+
+function paintQuestion(qnum) {
+    console.log(qArr[qnum].question);
+    $("#ptrivia").empty();
+    $("#ptrivia").append(qArr[qnum].question)
+}
+
+function paintChoice(qnum) {
+    var choices = qArr[qnum].choices
+    console.log(choices);
+    for (var i = 0; i < choices.length; i++) {
+        var choice = choices [i];
+       var pChoice = $("<p>").html(choice);
+        $("#ptrivia").append(pChoice);
+    }
+}
+
 
 
 
